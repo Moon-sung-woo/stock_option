7dae1dc6898a72d09758b0a571a358a484338990
msw <tjddn3322@naver.com>
2020-10-20 18:13:47 +0900

    index 바꿔주는거 추가

--- make_bollingerBand.py
+++ make_bollingerBand.py
@@ -3,11 +3,12 @@ import matplotlib.pyplot as plt
 
 w_size = 20 # 볼린져밴드 이동평균 산출 기간
 pb = 2      # 볼린져밴드 승수(percent bendwidth)
-stock_csv = '005380.csv'
+stock_csv = 'test.csv'
 
 data = pd.read_csv(stock_csv)
-range = pd.date_range('1990-01-04', '1991-01-01', freq='1D')
 end_data = data['종가']
+print(len(data))
+
 
 data['Moving Average'] = end_data.rolling(window=w_size, min_periods=1).mean()
 data['Standard Deviation'] = end_data.rolling(window=w_size, min_periods=1).std()
